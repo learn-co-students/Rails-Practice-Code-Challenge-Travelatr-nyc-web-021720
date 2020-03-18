47245305c959f5705248ed4a17d9de7ae6002acd
jguzz <joseph.a.guzzardo@gmail.com>
2020-03-18 16:45:41 -0400

    Adding Post functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,41 @@
 class PostsController < ApplicationController
     def index
+
     end
 
     def show
+        @post = Post.find(params[:id])
     end
     
     def new
+        @post = Post.new()
     end
 
     def create
+        @post = Post.create(post_params)
+
+        if @post.valid?
+            redirect_to @post
+        else  
+            flash[:errors] = @post.errors.full_messages
+            redirect_to new_post_path
+        end
     end
 
     def edit
+        @post = Post.find(params[:id])
     end
      
     def update
+        @post = Post.find(params[:id])
+        @post.update(params.require(:post).permit!)
+        redirect_to post_path(@post)
     end
 
     private
 
     def post_params
+        params.require(:post).permit!
     end
     
 end
