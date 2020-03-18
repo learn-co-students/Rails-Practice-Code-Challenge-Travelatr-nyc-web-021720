dac36ea4897532ba780cbee6314fd8440aeef9e0
github username <github email address>
2020-03-18 15:24:09 -0400

    Finished_Blogger

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -1,3 +1,23 @@
 class Blogger < ApplicationRecord
+    has_many :posts
+    has_many :destinations, through: :bloggers
+
+    validates :name, uniqueness: true
+    validates :age, numericality: {greater_than:0}
+    validates :bio, length: {minimum:30}
+
+    def total_like
+        total=0 
+         self.posts.each do |post| 
+            total+=post.likes
+         end
+         total
+    end
+
+    def most_like
+         self.posts.max_by{|p|p.likes}
+        #  byebug
+    end
+
 
 end
