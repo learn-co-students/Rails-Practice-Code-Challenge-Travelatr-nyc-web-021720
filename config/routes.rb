0684395f9cd424c5c1a3853b36dac2c20a9b9ead
matthewpeak <matthewpea@gmail.com>
2020-03-18 17:51:28 -0400

    put some text here

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :bloggers, only: [:create,:show,:index,:new]
+  resources :destinations, only: [:show]
+  resources :posts , only:[:edit,:update,:show,:create,:new]
 end
